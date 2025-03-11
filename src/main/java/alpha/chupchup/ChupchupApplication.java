191c507bfe961cf0e750175b64d984394685d8d7
didskfo <101495452+didskfo@users.noreply.github.com>
2025-03-11 17:42:27 +0900

    init

---
+++ src/main/java/alpha/chupchup/ChupchupApplication.java
@@ -0,0 +1,13 @@
+package alpha.chupchup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChupchupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChupchupApplication.class, args);
+	}
+
+}

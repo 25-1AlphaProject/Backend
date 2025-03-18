f7672817d791511f1575429adcc57d702459c39b
didskfo <didskfo2003@gmail.com>
2025-03-18 23:59:52 +0900

    FEAT: UserRecipeFavorite Class

---
+++ src/main/java/alpha/chupchup/entity/UserRecipeFavorite.java
@@ -0,0 +1,32 @@
+package alpha.chupchup.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "user_recipe_favorite")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class UserRecipeFavorite {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "recipe_id", nullable = false)
+    private Recipe recipe;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+}
+

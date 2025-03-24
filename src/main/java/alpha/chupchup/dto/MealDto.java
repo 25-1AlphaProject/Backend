f5dabd7daf392fae55d05736437eaba1794d02f1
didskfo <didskfo2003@gmail.com>
2025-03-24 23:43:59 +0900

    FEAT: MealDto, ResponseDto Add

---
+++ src/main/java/alpha/chupchup/dto/MealDto.java
@@ -0,0 +1,29 @@
+package alpha.chupchup.dto;
+
+import alpha.chupchup.entity.enums.MealType;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class MealDto {
+    String name;
+    String recipeImage;
+    String recipeText;
+    float calories;
+    float carbohydrates;
+    float protein;
+    float fat;
+    float sodium;
+    String foodImage;
+    String ingredient;
+    String foodType;
+    MealType mealType;
+    LocalDateTime dateTime;
+}

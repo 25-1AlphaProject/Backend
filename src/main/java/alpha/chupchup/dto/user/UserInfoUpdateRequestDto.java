485841c2c371021f3fbb6fc41a86e3e634628246
docherryra <docherryra@gmail.com>
2025-03-30 23:35:12 +0900

    FEAT: user-info request response dto

---
+++ src/main/java/alpha/chupchup/dto/user/UserInfoUpdateRequestDto.java
@@ -0,0 +1,11 @@
+package alpha.chupchup.dto.user;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserInfoUpdateRequestDto {
+    private String nickname;
+    private String profileImageUrl;
+}

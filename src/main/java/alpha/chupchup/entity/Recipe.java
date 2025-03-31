8444a23b0538bc564baa3f501715bb2f09e540e1
didskfo <didskfo2003@gmail.com>
2025-03-31 19:20:06 +0900

    CHORE: Recipe Text를 LONGTEXT로 변경

--- src/main/java/alpha/chupchup/entity/Recipe.java
+++ src/main/java/alpha/chupchup/entity/Recipe.java
@@ -24,7 +24,7 @@ public class Recipe {
     @Column(nullable = false, length = 100)
     private String name;
 
-    @Column(columnDefinition = "TEXT")
+    @Column(columnDefinition = "LONGTEXT")
     private String recipeText;
 
     private Float calories;

c03923ad78921ab6a31dfd192c18e8630453e54f
didskfo <didskfo2003@gmail.com>
2025-03-31 17:31:16 +0900

    FEAT: Swagger 설정

--- src/main/java/alpha/chupchup/controller/RecipeController.java
+++ src/main/java/alpha/chupchup/controller/RecipeController.java
@@ -4,6 +4,9 @@ import alpha.chupchup.dto.RecipeResponseDto;
 import alpha.chupchup.dto.RecipeSearchRequestDto;
 import alpha.chupchup.dto.ResponseDto;
 import alpha.chupchup.service.RecipeService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -15,12 +18,20 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/recipe")
 @RequiredArgsConstructor
+@Tag(name = "Recipe-Controller", description = "Recipe-관련-API")
 public class RecipeController {
 
     private final RecipeService recipeService;
 
     @PostMapping("/favorite/{recipeId}")
-    public ResponseEntity<ResponseDto<String>> postRecipeFavorite(@PathVariable Long recipeId) {
+    @Operation(summary = "레시피 좋아요 추가하기", description = "해당 레시피를 좋아요에 추가합니다.")
+    public ResponseEntity<ResponseDto<String>> postRecipeFavorite(
+            @Parameter(
+                    description = "레시피 아이디",
+                    required = true
+            )
+            @PathVariable Long recipeId
+    ) {
         try {
             recipeService.postRecipeFavorite(recipeId);
             return ResponseEntity.ok(ResponseDto.success("좋아요 추가에 성공했습니다."));
@@ -31,7 +42,14 @@ public class RecipeController {
     }
 
     @DeleteMapping("/favorite/{recipeId}")
-    public ResponseEntity<ResponseDto<String>> deleteRecipeFavorite(@PathVariable Long recipeId) {
+    @Operation(summary = "레시피 좋아요 삭제하기", description = "해당 레시피를 좋아요에서 삭제합니다.")
+    public ResponseEntity<ResponseDto<String>> deleteRecipeFavorite(
+            @Parameter(
+                    description = "레시피 아이디",
+                    required = true
+            )
+            @PathVariable Long recipeId
+    ) {
         try {
             recipeService.deleteRecipeFavorite(recipeId);
             return ResponseEntity.ok(ResponseDto.success("좋아요 삭제에 성공습니다."));
@@ -42,6 +60,7 @@ public class RecipeController {
     }
 
     @GetMapping("/favorite")
+    @Operation(summary = "좋아요 누른 레시피 조회하기", description = "좋아요를 눌렀던 레시피를 조회합니다.")
     public ResponseEntity<ResponseDto<?>> getFavoriteRecipe(HttpServletRequest servletRequest) {
         try {
             Long userId = 0L;
@@ -54,6 +73,7 @@ public class RecipeController {
     }
 
     @GetMapping("/search")
+    @Operation(summary = "레시피 검색하기", description = "키워드를 이용해서 관련된 레시피를 검색합니다.")
     public ResponseEntity<ResponseDto<?>> searchRecipe(@RequestBody RecipeSearchRequestDto requestDto) {
         try {
             String keyword = requestDto.getKeyword();
--- src/main/java/alpha/chupchup/dto/RecipeSearchRequestDto.java
+++ src/main/java/alpha/chupchup/dto/RecipeSearchRequestDto.java
@@ -1,5 +1,6 @@
 package alpha.chupchup.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,5 +9,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class RecipeSearchRequestDto {
+
+    @Schema(description = "검색할 키워드")
     private String keyword;
 }

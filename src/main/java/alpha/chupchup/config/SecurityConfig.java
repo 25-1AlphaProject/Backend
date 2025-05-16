426ee9580f567050e2b147dc712482f2e0c9062f
docherryra <docherryra@gmail.com>
2025-05-16 19:28:00 +0900

    fix: cors 설정 추가 (람다 DSL)

--- src/main/java/alpha/chupchup/config/SecurityConfig.java
+++ src/main/java/alpha/chupchup/config/SecurityConfig.java
@@ -37,6 +37,9 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
+                .cors(cors -> cors
+                        .configurationSource(corsConfigurationSource())
+                )
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth

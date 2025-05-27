ebe77e9f9e4ee2975a9618d0e9a96cfc13e0e92a
didskfo <didskfo2003@gmail.com>
2025-05-27 22:22:33 +0900

    FEAT: 레시피 생성 수정

--- src/main/java/alpha/chupchup/repository/MealRepository.java
+++ src/main/java/alpha/chupchup/repository/MealRepository.java
@@ -1,13 +1,14 @@
 package alpha.chupchup.repository;
 
 import alpha.chupchup.entity.recipe.WeeklyMeal;
+import alpha.chupchup.entity.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalDate;
 import java.util.List;
 
 public interface MealRepository extends JpaRepository<WeeklyMeal, Long> {
-    List<WeeklyMeal> findByUserIdAndMealDateBetweenOrderByMealDateAsc(Long userId, LocalDate startDay, LocalDate startDayTomorrow);
+    List<WeeklyMeal> findByUserAndMealDateBetweenOrderByMealDateAsc(User user, LocalDate startDate, LocalDate endDate);
 
     List<WeeklyMeal> findAllByUserIdAndMealDate(Long userId, LocalDate date);
 }
